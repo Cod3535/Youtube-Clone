c5bd2ee5dcc4c6c7f9973e05c13ceb249ad28d63
Cod3535 <114948870+Cod3535@users.noreply.github.com>
2024-04-15 10:45:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+nextParticle = new NextParticle(
+    nextParticle = new NextParticle({
+        image: document.all.logo,
+        addTimestamp: true,
+        initPosition: 'none',
+        initDirection: 'none',
+        
+      })
+);
